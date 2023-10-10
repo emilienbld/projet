b2826001753b24874b12b26975659bbab4cf018d
emilienbld <98468414+emilienbld@users.noreply.github.com>
2023-10-10 13:59:37 +0200

    Add files via upload

---
+++ Launcher.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+void Start() {
+	printf("Jeu de Roulette de Casino.\n");
+};
+
+int Menu() {
+	enum MenuButtons {
+		Nouvelle_partie = 1,
+		Quit
+	};
+};
+
+int End() {
+	End(0);
+};
